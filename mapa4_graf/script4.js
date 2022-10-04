d796fecc7179bb16c85d491a34618e31ac4989d1
MarkoKovac <marko.kovac.mk.2002@gmail.com>
2022-10-04 22:14:33 +0200

    cleaned code

--- mapa4_graf/script4.js
+++ mapa4_graf/script4.js
@@ -11,7 +11,7 @@ async function initMap() {
 
   const response = await fetch("../lokacije/lokacije4.json");
   const data = await response.json();
-  const start_location = { lat: 45.81295836043709, lng: 15.974741839933875};
+  const start_location = { lat: 45.81295836043709, lng: 15.974741839933875 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: start_location,
@@ -26,33 +26,33 @@ async function initMap() {
     content: "contentString",
   });
 
-  for (let i = 0; i < data.result.total; i++){
-      const name = data.result.records[i].detaljni_opis_lokacije;
-      const lon2 = data.result.records[i].Longitude;
-      const lat2 = data.result.records[i].Latitude;
-      const marker_location = { lat: lat2, lng: lon2};
+  for (let i = 0; i < data.result.total; i++) {
+    const name = data.result.records[i].detaljni_opis_lokacije;
+    const lon2 = data.result.records[i].Longitude;
+    const lat2 = data.result.records[i].Latitude;
+    const marker_location = { lat: lat2, lng: lon2 };
 
-      const infowindow = new google.maps.InfoWindow({
-        content: "<h3>" + name + "</h3>",
+    const infowindow = new google.maps.InfoWindow({
+      content: "<h3>" + name + "</h3>",
+    });
+
+    const marker = new google.maps.Marker({
+      position: marker_location,
+      map: map,
+      icon: {
+        url
+      }
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: false,
       });
-    
-      const marker = new google.maps.Marker({
-          position: marker_location,
-          map: map,
-          icon: {
-            url
-          }
-      });
-    
-      marker.addListener("click", () => {
-        infowindow.open({
-          anchor: marker,
-          map,
-          shouldFocus: false,
-        });
-      });
+    });
 
   }
 }
-  
+
 window.initMap = initMap;
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 async function initMap() {
-  
-    const response2 = await fetch('dizajn.json');
-    const data2 = await response2.json();
-    const styledMapType = new google.maps.StyledMapType(
-      data2,
-      { name: "Styled Map" }
-    );
-  
-    const start_location = { lat: 45.81295836043709, lng: 15.974741839933875};
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
-      center: start_location,
-      disableDefaultUI: true,
-      keyboardShortcuts: false
-    });
-  
-    map.mapTypes.set("styled_map", styledMapType);
-    map.setMapTypeId("styled_map");
-  
+
+  const response2 = await fetch('dizajn.json');
+  const data2 = await response2.json();
+  const styledMapType = new google.maps.StyledMapType(
+    data2,
+    { name: "Styled Map" }
+  );
+
+  const start_location = { lat: 45.81295836043709, lng: 15.974741839933875 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: start_location,
+    disableDefaultUI: true,
+    keyboardShortcuts: false
+  });
+
+  map.mapTypes.set("styled_map", styledMapType);
+  map.setMapTypeId("styled_map");
+
 }
-    
-  window.initMap = initMap;
+
+window.initMap = initMap;
